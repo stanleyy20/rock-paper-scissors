6a2bc5726fd740d60bf4097346945d8457b5869e
Dawid Stach <dstach20@gmail.com>
2022-07-22 14:54:59 +0200

    added pwa plugin and manifest file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     base: '/rock-paper-scissors/',
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true,
+            },
+        }),
+    ],
 });
